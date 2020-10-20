f58449f73cd97b4d5fdd3188102c831d1ddff774
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:43:55 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan6-Kambing Static&Konstanta/src/if1/pkg10117126/latihan6/kambing/pkgstatic/konstanta/IF110117126Latihan6KambingStaticKonstanta.java
@@ -0,0 +1,28 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan6.kambing.pkgstatic.konstanta;
+
+/**
+ *
+ * @author User
+ */
+public class IF110117126Latihan6KambingStaticKonstanta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static final String NAMA_KAMBING = "Midun";
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+    
+}
---
+++ IF1-10117126-Latihan6-Kambing Static&Konstanta/src/if1/pkg10117126/latihan6/kambing/pkgstatic/konstanta/Mamalia.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan6.kambing.pkgstatic.konstanta;
+
+/**
+ *
+ * @author User
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+    
+}
